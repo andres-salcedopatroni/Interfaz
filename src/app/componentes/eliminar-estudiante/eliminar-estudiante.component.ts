76f594c591c0852794cb8ce4fcf4e32ebf0be0eb
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-02-03 00:20:05 -0500

    Nuevo

--- src/app/componentes/eliminar-estudiante/eliminar-estudiante.component.ts
+++ src/app/componentes/eliminar-estudiante/eliminar-estudiante.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EliminarEstudianteComponent implements OnInit {
 
-  constructor() { }
+  usuarios:any;
+  resultados:any;
+  lista_id:any=[];
+  
+  constructor(private servicioUsuario: UsersService,private servicioAnalisis: AnalisisService) { 
+
+    this.servicioUsuario.obtenerUsuarios().subscribe(
+      (data)=> {
+        this.usuarios=data
+      },(error)=> {
+        console.log(error)
+      }   
+    )
+
+  }
 
   ngOnInit(): void {
   }
 
+  analizar(): void{
+    
+    const field:any="usuario"
+    for (let u of this.usuarios) {
+      let user:any=u
+      this.lista_id.push( user.usuario)
+      console.log(this.usuarios)
+      console.log(this.lista_id)
+    }
+    this.servicioAnalisis.analizar({"usuarios":this.lista_id}).subscribe(
+      (data)=> {
+        this.resultados=data
+        console.log(this.resultados)
+      },(error)=> {
+        console.log(error)
+      }   
+    )
+  }
 }
