0b1644a4a69d1d07dd03a31e85034a6008554b09
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-02-09 20:20:01 -0500

    Nuevo

--- src/app/componentes/modificar-estudiante/modificar-estudiante.component.ts
+++ src/app/componentes/modificar-estudiante/modificar-estudiante.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-modificar-estudiante',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./modificar-estudiante.component.css']
 })
 export class ModificarEstudianteComponent {
+  usuarios:any;
+  resultados:any;
+  lista_id:any=[];
+  
+  constructor(private servicioUsuario: UsersService) { 
 
+    this.servicioUsuario.obtenerUsuarios().subscribe(
+      (data)=> {
+        this.usuarios=data
+      },(error)=> {
+        console.log(error)
+      }   
+    )
+
+  }
+
+  ngOnInit(): void {
+  }
 }
