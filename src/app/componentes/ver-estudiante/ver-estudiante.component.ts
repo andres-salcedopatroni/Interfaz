63c06f11b8a4aac2fefbab1b98ff78199817beed
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-03-04 00:25:50 -0500

    Nuevo

--- src/app/componentes/ver-estudiante/ver-estudiante.component.ts
+++ src/app/componentes/ver-estudiante/ver-estudiante.component.ts
@@ -51,6 +51,70 @@ export class VerEstudianteComponent implements OnInit {
       }
     ]
   }
+  pieChartOptions:any={
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Browser market shares in May, 2020',
+        align: 'left'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Chrome',
+            y: 70.67,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Edge',
+            y: 14.77
+        },  {
+            name: 'Firefox',
+            y: 4.86
+        }, {
+            name: 'Safari',
+            y: 2.63
+        }, {
+            name: 'Internet Explorer',
+            y: 1.53
+        },  {
+            name: 'Opera',
+            y: 1.40
+        }, {
+            name: 'Sogou Explorer',
+            y: 0.84
+        }, {
+            name: 'QQ',
+            y: 0.51
+        }, {
+            name: 'Other',
+            y: 2.6
+        }]
+    }]
+}
 
   constructor(private router:Router, private servicioUsuario: UsersService, private route: ActivatedRoute) { 
 
@@ -86,6 +150,7 @@ export class VerEstudianteComponent implements OnInit {
     console.log(this.graficoDatos);
     
    Highcharts.chart('grafica', this.options); 
+   Highcharts.chart('pieChart', this.pieChartOptions);
   }
 
   
